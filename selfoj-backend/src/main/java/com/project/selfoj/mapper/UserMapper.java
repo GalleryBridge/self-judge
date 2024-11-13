7571b19366159d6172b890af57d3232821d72eac
Laidor <Laidoring@163.com>
2024-11-13 21:51:23 +0800

    feat: 初始化后端

---
+++ selfoj-backend/src/main/java/com/project/selfoj/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.project.selfoj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.project.selfoj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * 
+ * 
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
